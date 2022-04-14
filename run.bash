ef4a1331d53dbba76bd8a3ea7bbe130937b96ee3
Ronald Portier <ronald@portier.eu>
2022-04-14 23:38:17 +0200

    [RFR] Use pip and setuptools instead of poetry and docker

---
+++ run.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+uvicorn fastapi_blog.app:app --host 0.0.0.0 --port 80
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="fastapi-blog",
+    version="0.1.0",
+    description="FastApi App for Web Blogging",
+    url="https://github.com/shuds13/pyexample",
+    author="Smetanin Aleksandr",
+    author_email="smetamx@gmail.com",
+    license="BSD 2-clause",
+    packages=["fastapi_blog"],
+)
